aa419e1249b7c69443ee8f3afcc8169615a49b7d
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2023-12-12 15:30:47 +0100

    Initial commit

---
+++ src/main/java/org/example/dao/EventsDAO.java
@@ -0,0 +1,11 @@
+package org.example.dao;
+
+import org.example.entities.Event;
+
+import javax.persistence.EntityManager;
+
+public class EventDAO {
+    private final EntityManager em;
+
+    public Event
+}
